05e855c7909f1800e37af9979b973f489110b9d7
诗无尽头i <svipchao@qq.com>
2022-06-15 08:42:47 +0800

    新增编辑器

---
+++ vue-admin/src/components/vditor/edit.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="vditor" />
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { assignObject } from "@/utils/utils.js";
+import Vditor from "vditor";
+import "vditor/dist/index.css";
+
+const props = defineProps({
+  options: {},
+});
+
+const vditor = ref(null);
+
+onMounted(() => {
+  vditor.value = new Vditor(
+    "vditor",
+    assignObject(
+      {
+        cache: {
+          enable: false,
+        },
+        mode: "wysiwyg",
+        placeholder: "内容",
+        cdn: "vditor",
+        width: "100%",
+        height: 200,
+        toolbar: [
+          "headings",
+          "bold",
+          "italic",
+          "strike",
+          "link",
+          "|",
+          "list",
+          "ordered-list",
+          "check",
+          "outdent",
+          "indent",
+          "|",
+          "quote",
+          "line",
+          "code",
+          "inline-code",
+          "insert-before",
+          "insert-after",
+          "|",
+          "upload",
+          "table",
+          "|",
+          "fullscreen",
+        ],
+      },
+      props.options
+    )
+  );
+});
+
+defineExpose({ vditor });
+</script>
