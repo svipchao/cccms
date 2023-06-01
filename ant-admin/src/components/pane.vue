4fb4a4b3b9fff93f0f1d4c676cb477ad8a22fe36
诗无尽头i <admin@cccms.cc>
2023-06-01 23:31:03 +0800

    init .

--- ant-admin/src/components/pane.vue
+++ ant-admin/src/components/pane.vue
@@ -1,40 +1,240 @@
 <template>
-  <div class="cc-pane">
-    <div class="cc-pane-left"></div>
-    <div class="cc-pane-right"></div>
+  <div :class="dark ? 'dark' : 'default'">
+    <a-config-provider
+      :theme="{
+        algorithm: dark ? theme.darkAlgorithm : theme.defaultAlgorithm,
+      }"
+      :space="{ size: 'middle' }"
+    >
+      <a-card size="small">
+        <div class="cc-pane">
+          <div
+            class="cc-pane-left"
+            :style="{
+              marginLeft: siderShow ? '0px' : '-300px',
+            }"
+          >
+            <span class="cc-pane-split" @click="siderSwitch">
+              <i class="ri-arrow ri-arrow-left-s-fill"></i>
+            </span>
+          </div>
+          <div class="cc-pane-right">
+            <a-button @click="darkSwitch">切换</a-button>
+            <a-table
+              :dataSource="dataSource"
+              :columns="columns"
+              :scroll="{ x: 1500 }"
+            />
+          </div>
+        </div>
+      </a-card>
+    </a-config-provider>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, reactive } from "vue";
+import { theme } from "ant-design-vue";
+
+const dark = ref(false);
+const darkSwitch = () => {
+  dark.value = !dark.value;
+};
+
+const siderShow = ref(true);
+const siderSwitch = () => {
+  siderShow.value = !siderShow.value;
+};
+
+const dataSource = [
+  {
+    key: "1",
+    name: "胡彦斌",
+    age: 32,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+];
+
+const columns = [
+  {
+    title: "姓名",
+    dataIndex: "name",
+    key: "name",
+    width: 200,
+  },
+  {
+    title: "年龄",
+    dataIndex: "age",
+    key: "age",
+    width: 200,
+  },
+  {
+    title: "住址",
+    dataIndex: "address",
+    key: "address",
+    width: 200,
+  },
+];
+</script>
 
 <style lang="less">
 .cc-pane {
-  display: flex;
   height: 500px;
-  border: 1px solid red;
+  display: flex;
+  overflow: hidden;
   .cc-pane-left {
     flex: 0 0 300px;
-    border: 1px solid blue;
+    overflow: auto;
+    padding: 12px;
+    border-radius: 8px 0 0 8px;
+    border: 1px solid var(--cc-border-color);
+    border-right: 0px;
+    .cc-pane-split {
+      z-index: 200;
+      margin-left: 288px;
+      position: absolute;
+      .ri-arrow-left-s-fill,
+      .ri-arrow-right-s-fill {
+        color: var(--cc-text-color);
+        width: 14px;
+        cursor: pointer;
+        font-size: 12px;
+        height: 30px;
+        line-height: 30px;
+        z-index: 997;
+        display: block;
+        border-radius: 0 4px 4px 0;
+        background: var(--cc-background-color);
+        border-top: 0px;
+        border-left: 0px;
+        &:hover {
+          z-index: 100;
+          background: var(--cc-background-hover-color);
+        }
+      }
+    }
   }
   .cc-pane-right {
-    flex: 1 1 auto;
     width: 300px;
-    border: 1px solid var(--cc-color);
+    padding: 12px;
+    flex: 1 1 auto;
+    overflow: auto;
+    border-radius: 0 8px 8px 0;
+    border: 1px solid var(--cc-border-color);
   }
-}
-.dark {
-  --cc-color: #d9d9d9;
-}
-.default {
-  --cc-color: #424242;
-}
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-}
-body {
-  background: #f2f2f2;
-  padding: 10px;
 }
 </style>
--- ant-admin/src/main.js
+++ ant-admin/src/main.js
@@ -4,6 +4,8 @@ import App from "./App.vue";
 import Antd from "ant-design-vue";
 import { createPersistedState } from "pinia-plugin-persistedstate";
 import "ant-design-vue/dist/reset.css";
+import "@/assets/vars.less";
+// import "@/assets/app.less";
 import "@/assets/remixicon/remixicon.css";
 
 const app = createApp(App);
