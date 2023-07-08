0ba89dd5b721881f0a93dd86af1c9bcad7d866dc
诗无尽头i <svipchao@qq.com>
2023-07-08 17:10:40 +0800

    init .

--- ant-admin/src/config.js
+++ ant-admin/src/config.js
@@ -1,15 +1,14 @@
 export default {
-    // 标题
-    title: "CCCMS",
-    // 描述
-    description: "CCCMS后台管理系统",
-    // 版权链接
-    copyrightTitle: "CCCMS",
-    // 版权链接
-    copyrightUrl: "https://www.cccms.cc/",
-    // 接口URL
-    baseUrl: "http://cms.qq.com",
-    // 全局确认框加载时间 ms
-    okLoadingTime: 1000,
-  };
-  
+  // 标题
+  title: "CCCMS",
+  // 描述
+  description: "CCCMS后台管理系统",
+  // 版权链接
+  copyrightTitle: "CCCMS",
+  // 版权链接
+  copyrightUrl: "https://www.cccms.cc/",
+  // 接口URL
+  baseUrl: "http://cms.qq.com",
+  // 全局确认框加载时间 ms
+  okLoadingTime: 1000,
+};
