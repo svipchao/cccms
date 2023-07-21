b0484f7aa2bc1a0bd5b44c0eb1fe445e2d84c2ad
诗无尽头i <svipchao@qq.com>
2023-07-21 16:41:04 +0800

    添加插件系统

---
+++ addons-cccms/addons/init/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace addons\init\controller;
+
+class Index
+{
+    public function index()
+    {
+        _result([], 'index');
+    }
+
+    public function _empty()
+    {
+        echo '空方法';
+    }
+}
