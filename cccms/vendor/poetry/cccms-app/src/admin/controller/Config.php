2f69d3e2163af1f6766711e64034a8fe405dac21
诗无尽头i <svipchao@qq.com>
2023-10-27 13:36:29 +0800

    init .

--- cccms/vendor/poetry/cccms-app/src/admin/controller/Config.php
+++ cccms/vendor/poetry/cccms-app/src/admin/controller/Config.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace app\admin\controller;
 
-use app\admin\model\SysConfig;
+use cccms\model\SysConfig;
 use cccms\Base;
 use cccms\services\{AuthService, TypesService};
 
