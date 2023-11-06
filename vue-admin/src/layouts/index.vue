ae2ffec8be15b1484c122ede31a0fbcb94ca3413
诗无尽头i <svipchao@qq.com>
2023-11-06 18:06:33 +0800

    init .

--- vue-admin/src/layouts/index.vue
+++ vue-admin/src/layouts/index.vue
@@ -1,37 +1,39 @@
 <template>
-  <a-layout class="cccms-layout" v-waterMarker="water">
-    <a-layout-sider
-      :style="{ display: themeStore.getShowSider ? 'block' : 'none' }"
-    >
-      <Sider />
-    </a-layout-sider>
-    <a-layout>
-      <a-layout-header v-show="!themeStore.contentFullscreen">
-        <Header />
-      </a-layout-header>
+  <a-watermark :content="watermarkText" :zIndex="99999" :gap="[50, 50]">
+    <a-layout class="cccms-layout">
+      <a-layout-sider
+        :style="{ display: themeStore.getShowSider ? 'block' : 'none' }"
+      >
+        <Sider />
+      </a-layout-sider>
       <a-layout>
-        <a-layout-content>
-          <div class="cccms-tabs">
-            <Tabs />
-          </div>
-          <router-view v-slot="{ Component }">
-            <Transition duration="500" name="nested">
-              <div class="cccms-content" v-if="!tabsStore.isRefresh">
-                <keep-alive :include="tabsStore.getCacheTabs">
-                  <component :is="Component" />
-                </keep-alive>
-              </div>
-            </Transition>
-          </router-view>
-        </a-layout-content>
-        <div
-          class="cccms-mark"
-          @click="themeStore.switchShowSider()"
-          v-show="themeStore.showSider"
-        />
+        <a-layout-header v-show="!themeStore.contentFullscreen">
+          <Header />
+        </a-layout-header>
+        <a-layout>
+          <a-layout-content>
+            <div class="cccms-tabs">
+              <Tabs />
+            </div>
+            <router-view v-slot="{ Component }">
+              <Transition duration="500" name="nested">
+                <div class="cccms-content" v-if="!tabsStore.isRefresh">
+                  <keep-alive :include="tabsStore.getCacheTabs">
+                    <component :is="Component" />
+                  </keep-alive>
+                </div>
+              </Transition>
+            </router-view>
+          </a-layout-content>
+          <div
+            class="cccms-mark"
+            @click="themeStore.switchShowSider()"
+            v-show="themeStore.showSider"
+          />
+        </a-layout>
       </a-layout>
     </a-layout>
-  </a-layout>
+  </a-watermark>
 </template>
 
 <script setup>
@@ -42,15 +44,20 @@ import Tabs from './content/tabs.vue';
 import { useTabs } from '@/store/admin/tabs.js';
 import { useUser } from '@/store/admin/user.js';
 import { useTheme } from '@/store/admin/theme.js';
+import { timestamp } from '@/utils/time.js';
 
 const tabsStore = useTabs();
 const userStore = useUser();
 const themeStore = useTheme();
 
-const water = reactive({
-  text: '',
-});
-water.text = `@ID:${userStore.id} ${userStore.nickname}(${userStore.username})`;
+const watermarkText = [
+  `@ID:${userStore.id} ${userStore.nickname}(${userStore.username})`,
+  timestamp(),
+];
+// const water = reactive({
+//   text: '',
+// });
+// water.text = `@ID:${userStore.id} ${userStore.nickname}(${userStore.username})`;
 </script>
 
 <style lang="less">
